6ab00e544ec5af73f75cec05a38a1fb6d1cfd7ac
Mathieu Blondel <mathieu@mblondel.org>
2011-09-04 04:03:48 +0900

    Add create_symlinks.ssh.

---
+++ create_symlinks.sh
@@ -0,0 +1,11 @@
+CURRDIR="$( cd -P "$( dirname "$0" )" && pwd )"
+
+for file in `ls -a $CURRDIR | egrep "^\.[a-zA-Z]" | grep -v ssh`; do
+    rm -rf $HOME/$file
+    ln -s $CURRDIR/$file $HOME/$file
+done
+
+rm -rf $HOME/.git
+rm -f $HOME/.gitmodules
+rm -f $HOME/.ssh/config
+ln -s $CURRDIR/.ssh/config $HOME/.ssh/config 
